a20c8e05e3c25c1ec84f1c02e1034aa3c152674b
Dongliang Leng <Dongliang.Leng@royole.com>
2018-08-14 17:26:28 +0800

    修改过滤

--- iOSConfuse/confuse/confuseHeader.h
+++ iOSConfuse/confuse/confuseHeader.h
@@ -1,10 +1,10 @@
 #ifndef confuse_header_h
 #define confuse_header_h
-//confuse string at Tue Aug 14 17:08:25 CST 2018
-#define myTest FDRjgtvlErbkAPeK
-#define staticTest ConwVXNHSWMNzLMl
-#define test GieXVwEPCGIPgEUk
-#define AppDelegate NNoSqssTUUdoQXsr
-#define Test XMRSlOGzVEkstwcm
-#define ViewController sihPBKAXbLgyHOAi
+//confuse string at Tue Aug 14 17:24:37 CST 2018
+#define myTest mILFfXwZJoklGfjJ
+#define staticTest lMHfglBlzVZGmIkk
+#define test NCZynbRSomuzgQVu
+#define AppDelegate ZolQjQwMFrAIcExR
+#define Test aFcaXSwUFDYBWESG
+#define ViewController VfrKfbDMKRaRJHQI
 #endif
